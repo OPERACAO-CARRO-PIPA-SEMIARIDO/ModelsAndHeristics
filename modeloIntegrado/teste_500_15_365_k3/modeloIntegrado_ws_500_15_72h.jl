7848165c514b47210b76e7e67184fe02be688d08
Guilherme Ramos <guilhermebentoramos@gmail.com>
2026-06-21 17:26:56 -0300

    feat: adiciona teste reduzido 500x15

---
+++ modeloIntegrado/teste_500_15_365_k3/modeloIntegrado_ws_500_15_72h.jl
@@ -0,0 +1,327 @@
+using JuMP
+using LinearAlgebra
+using CSV
+using DataFrames
+using Gurobi
+using MathOptInterface
+const MOI = MathOptInterface
+
+const BASE_PATH = "C:/Users/lfeli/Documents/AlocacaoCarros/dados/"
+const TEST_DIR = @__DIR__
+const TOTAL_MANANCIAIS_ARQUIVO = 92
+const CAPACIDADE_MAX_MANANCIAL = 12
+
+const TOTAL_BENEFICIARIOS = 500
+const TOTAL_MANANCIAIS = 15
+const TOTAL_DIAS = 365
+const NUM_CANDIDATOS = 3
+
+beneficiarios_ativos = CSV.read(BASE_PATH * "Beneficiarios_RN_Ativos1.csv", DataFrame)
+dias_uteis = CSV.read(BASE_PATH * "datas.csv", DataFrame)
+calendarios = CSV.read(BASE_PATH * "CalendariosObrigatorios.csv", DataFrame)
+rotas = CSV.read(BASE_PATH * "rotas", DataFrame)
+
+calendarioCarnaval = calendarios.carnaval
+entregasObrigatorias = calendarios.lil
+
+TOTAL_BENEFICIARIOS_ARQUIVO = size(beneficiarios_ativos, 1)
+
+nb = 1:TOTAL_BENEFICIARIOS
+nd = 1:TOTAL_DIAS
+nm = 1:TOTAL_MANANCIAIS
+
+qtd_dias_uteis = sum(dias_uteis[nd, 1])
+
+preU_full = [round(i * 0.02, digits=2) for i in beneficiarios_ativos.Pessoas_Atendidas]
+U = preU_full[nb]
+
+C_full = convert(Vector{Float64}, beneficiarios_ativos.Capacidade)
+C = C_full[nb]
+
+Y = C ./ U
+quebra4 = [j for (j, x) in zip(nb, Y) if x < 5]
+quebra2 = [j for (j, x) in zip(nb, Y) if x < 3]
+
+distancias = rotas.distance_w_factor
+Dij_completa = transpose(reshape(distancias, (TOTAL_BENEFICIARIOS_ARQUIVO, TOTAL_MANANCIAIS_ARQUIVO)))
+Dij = Dij_completa[nm, nb]
+
+CANDIDATOS_REAIS = min(NUM_CANDIDATOS, TOTAL_MANANCIAIS)
+candidatos_por_beneficiario = Dict{Int, Vector{Int}}()
+for j in nb
+    fontes_ordenadas = sortperm(Dij[:, j])
+    candidatos_por_beneficiario[j] = fontes_ordenadas[1:CANDIDATOS_REAIS]
+end
+
+function rodar_modelo_integrado(p::Float64, nome_pasta::String;
+                                abastecimento_warm_start=nothing,
+                                alocacao_warm_start=nothing)
+    caminho_pasta = isabspath(nome_pasta) ? nome_pasta : joinpath(TEST_DIR, nome_pasta)
+    if !isdir(caminho_pasta)
+        mkpath(caminho_pasta)
+    end
+
+    model = Model(Gurobi.Optimizer)
+    set_optimizer_attribute(model, "OutputFlag", 1)
+    set_optimizer_attribute(model, "LogToConsole", 1)
+    set_optimizer_attribute(model, "NodefileStart", 10.0)
+    set_optimizer_attribute(model, "MemLimit", 28.0)
+    set_optimizer_attribute(model, "Threads", 4)
+    set_optimizer_attribute(model, "MIPFocus", 1)
+
+    @variable(model, 0 <= x[j in nb, i in candidatos_por_beneficiario[j], k in nd], Int)
+    @variable(model, z[j in nb, i in candidatos_por_beneficiario[j]], Bin)
+    @variable(model, 0 <= y_pico, Int)
+    @variable(model, 0 <= V[j in nb, k in 0:last(nd)])
+
+    @expression(model, expr_pico, qtd_dias_uteis * y_pico)
+    @expression(model, expr_custo, sum(Dij[i, j] * x[j, i, k] for j in nb, i in candidatos_por_beneficiario[j], k in nd))
+    @expression(model, expr_entregas, sum(x[j, i, k] for j in nb, i in candidatos_por_beneficiario[j], k in nd))
+
+    @objective(model, Min, (p * expr_pico) + ((1 - p) * expr_custo))
+
+    @constraint(model, balancoVolumeInicial[j in nb], V[j, 0] == C[j])
+
+    @constraint(model, balancoVolume[j in nb, k in 1:last(nd);
+            !(calendarioCarnaval[k] == -1 && j in quebra4) &&
+            !(entregasObrigatorias[k] == -1 && j in quebra2)],
+        V[j, k] <= V[j, k - 1] - U[j] + 13.0 * sum(x[j, i, k] for i in candidatos_por_beneficiario[j]))
+
+    @constraint(model, correcaoVolume[j in nb, k in nd;
+            (calendarioCarnaval[k] == -1 && j in quebra4) ||
+            (entregasObrigatorias[k] == -1 && j in quebra2)],
+        V[j, k] == 0)
+
+    @constraint(model, diasInuteis[j in nb, k in nd; Int(dias_uteis[k, 1]) == 0],
+        sum(x[j, i, k] for i in candidatos_por_beneficiario[j]) == 0)
+
+    @constraint(model, restMaiorPico[k in nd],
+        sum(x[j, i, k] for j in nb, i in candidatos_por_beneficiario[j]) <= y_pico)
+
+    @constraint(model, volumeMinimo[j in nb, k in 0:last(nd)], V[j, k] >= 0)
+    @constraint(model, capacidadeMax[j in nb, k in 0:last(nd)], V[j, k] <= C[j])
+
+    @constraint(model, carnavalAbastecimento[j in quebra4, k in nd; calendarioCarnaval[k] == 1],
+        sum(x[j, i, k] for i in candidatos_por_beneficiario[j]) >= 1)
+
+    @constraint(model, lilAbastecimento[j in quebra2, k in nd; entregasObrigatorias[k] == 1],
+        sum(x[j, i, k] for i in candidatos_por_beneficiario[j]) >= 1)
+
+    @constraint(model, fonteUnica[j in nb],
+        sum(z[j, i] for i in candidatos_por_beneficiario[j]) == 1)
+
+    @constraint(model, amarra_z_x[j in nb, k in nd, i in candidatos_por_beneficiario[j]],
+        x[j, i, k] <= CAPACIDADE_MAX_MANANCIAL * z[j, i])
+
+    @constraint(model, capDiariaManancial[i in nm, k in nd;
+            !isempty([j for j in nb if i in candidatos_por_beneficiario[j]])],
+        sum(x[j, i, k] for j in nb if i in candidatos_por_beneficiario[j]) <= CAPACIDADE_MAX_MANANCIAL)
+
+    z_hint = Dict{Int, Int}(j => candidatos_por_beneficiario[j][1] for j in nb)
+    n_heu_usados = 0
+
+    if !isnothing(alocacao_warm_start) && isfile(alocacao_warm_start)
+        try
+            df_aloc = CSV.read(alocacao_warm_start, DataFrame)
+            for row_idx in 1:size(df_aloc, 1)
+                j_id = hasproperty(df_aloc, :Beneficiarios) ? df_aloc[row_idx, :Beneficiarios] : df_aloc[row_idx, 1]
+                if !(j_id in nb)
+                    continue
+                end
+                for k in nd
+                    col_sym = Symbol(string(k))
+                    if hasproperty(df_aloc, col_sym)
+                        val_i = df_aloc[row_idx, col_sym]
+                        if !ismissing(val_i) && val_i > 0
+                            fonte = Int(val_i)
+                            if fonte in candidatos_por_beneficiario[j_id]
+                                z_hint[j_id] = fonte
+                                n_heu_usados += 1
+                            end
+                            break
+                        end
+                    end
+                end
+            end
+        catch e
+            println(">>> Aviso warm start z: $e")
+        end
+    end
+
+    for j in nb
+        fonte = z_hint[j]
+        set_start_value(z[j, fonte], 1.0)
+        for i in candidatos_por_beneficiario[j]
+            if i != fonte
+                set_start_value(z[j, i], 0.0)
+            end
+        end
+    end
+
+    if !isnothing(abastecimento_warm_start) && isfile(abastecimento_warm_start)
+        try
+            df_abast = CSV.read(abastecimento_warm_start, DataFrame)
+            pico_ws = 0
+            for row_idx in 1:size(df_abast, 1)
+                j_id = hasproperty(df_abast, :Beneficiarios) ? df_abast[row_idx, :Beneficiarios] : df_abast[row_idx, 1]
+                if !(j_id in nb)
+                    continue
+                end
+                fonte = z_hint[j_id]
+                for k in nd
+                    col_sym = Symbol(string(k))
+                    if hasproperty(df_abast, col_sym)
+                        val_x = df_abast[row_idx, col_sym]
+                        if !ismissing(val_x) && val_x > 0
+                            set_start_value(x[j_id, fonte, k], Float64(val_x))
+                        end
+                    end
+                end
+            end
+            for k in nd
+                col_sym = Symbol(string(k))
+                if hasproperty(df_abast, col_sym)
+                    soma = sum(skipmissing(df_abast[!, col_sym]))
+                    if soma > pico_ws
+                        pico_ws = round(Int, soma)
+                    end
+                end
+            end
+            if pico_ws > 0
+                set_start_value(y_pico, Float64(pico_ws))
+            end
+        catch e
+            println(">>> Aviso warm start x: $e")
+        end
+    end
+
+    println(">>> Warm start: heuristica/minpicos aplicada em $n_heu_usados beneficiarios.")
+
+    horas_checkpoints = 3:3:72
+    segundos_checkpoints = Float64.(horas_checkpoints .* 3600)
+
+    df_historico = DataFrame(
+        Hora=Int[],
+        Tempo_Segundos=Float64[],
+        Objective_HigherBound=Float64[],
+        Best_LowerBound=Float64[],
+        Gap_Percent=Float64[],
+        Pico_Y=Int[],
+        Custo_Roteamento=Float64[],
+        Qtd_Entregas=Int[],
+    )
+
+    tempo_acumulado = 0.0
+    melhor_obj_encontrado = Inf
+
+    for (hora, meta_tempo) in zip(horas_checkpoints, segundos_checkpoints)
+        tempo_restante = meta_tempo - tempo_acumulado
+        if tempo_restante <= 0
+            continue
+        end
+
+        set_optimizer_attribute(model, "TimeLimit", tempo_restante)
+
+        try
+            optimize!(model)
+        catch e
+            if isa(e, InterruptException)
+                println("Execucao interrompida manualmente na hora $hora.")
+                if has_values(model)
+                    salvar_saidas(model, caminho_pasta, "$(hora)h_INT")
+                end
+                return
+            else
+                println("\nErro Fatal: $e")
+                break
+            end
+        end
+
+        tempo_acumulado = meta_tempo
+
+        if has_values(model)
+            obj = objective_value(model)
+            bound = try
+                objective_bound(model)
+            catch
+                -1.0
+            end
+            gap = try
+                MOI.get(model, MOI.RelativeGap()) * 100
+            catch
+                0.0
+            end
+
+            val_pico = round(Int, value(y_pico))
+            val_custo = value(expr_custo)
+            val_entregas = round(Int, value(expr_entregas))
+
+            push!(df_historico, (hora, tempo_acumulado, obj, bound, gap, val_pico, val_custo, val_entregas))
+            CSV.write(joinpath(caminho_pasta, "historico_controle.csv"), df_historico)
+
+            if obj < melhor_obj_encontrado
+                println(">>> Melhor solucao encontrada na hora $hora: Obj = $obj")
+                melhor_obj_encontrado = obj
+                salvar_saidas(model, caminho_pasta, "$(hora)h")
+                salvar_saidas(model, caminho_pasta, "melhor_absoluto")
+            end
+        else
+            println(">>> Hora $hora: sem solucao viavel ainda.")
+        end
+
+        if termination_status(model) == MOI.OPTIMAL
+            println("Solucao otima comprovada. Finalizando.")
+            break
+        end
+    end
+end
+
+function salvar_saidas(model, pasta, sufixo)
+    val_x = value.(model[:x])
+    val_z = value.(model[:z])
+
+    colunas_abastecimento = Any[[j for j in nb]]
+    colunas_alocacao = Any[[j for j in nb]]
+
+    for k in nd
+        arr_abast_dia = Int[]
+        arr_aloc_dia = Int[]
+        for j in nb
+            fonte_escolhida = 0
+            for i in candidatos_por_beneficiario[j]
+                if val_z[j, i] > 0.5
+                    fonte_escolhida = i
+                    break
+                end
+            end
+            soma_caminhoes = sum(round(Int, val_x[j, i, k]) for i in candidatos_por_beneficiario[j])
+            push!(arr_abast_dia, soma_caminhoes)
+            push!(arr_aloc_dia, soma_caminhoes > 0 ? fonte_escolhida : 0)
+        end
+        push!(colunas_abastecimento, arr_abast_dia)
+        push!(colunas_alocacao, arr_aloc_dia)
+    end
+
+    nomes_colunas = Symbol.(["Beneficiarios"; string.(collect(nd))])
+
+    df_abastecimento = DataFrame(colunas_abastecimento, nomes_colunas)
+    CSV.write(joinpath(pasta, "abastecimento_$sufixo.csv"), df_abastecimento)
+
+    df_alocacao = DataFrame(colunas_alocacao, nomes_colunas)
+    CSV.write(joinpath(pasta, "alocacao_$sufixo.csv"), df_alocacao)
+end
+
+ws_abast_opt = joinpath(TEST_DIR, "resultados_minpicos_p00", "abastecimento_melhor_absoluto.csv")
+ws_aloc_opt = joinpath(TEST_DIR, "alocacao_m2_minpicos_p00.csv")
+ws_abast_heu = joinpath(TEST_DIR, "abastecimento_heu_limite.csv")
+ws_aloc_heu = joinpath(TEST_DIR, "alocacao_heu_limite.csv")
+
+ws_abast = isfile(ws_abast_opt) ? ws_abast_opt : ws_abast_heu
+ws_aloc = isfile(ws_aloc_opt) ? ws_aloc_opt : ws_aloc_heu
+
+println("Warm start abastecimento: $ws_abast")
+println("Warm start alocacao:      $ws_aloc")
+
+rodar_modelo_integrado(0.00, "resultados_500_15_365_72h";
+    abastecimento_warm_start=ws_abast,
+    alocacao_warm_start=ws_aloc)
---
+++ modeloIntegrado/teste_500_15_365_k3/preparar_teste_reduzido.py
@@ -0,0 +1,129 @@
+import os
+import subprocess
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+
+REPO_ROOT = Path(r"C:\Users\lfeli\Documents\AlocacaoCarros\ModelsAndHeristics")
+DATA_ROOT = Path(r"C:\Users\lfeli\Documents\AlocacaoCarros\dados")
+TEST_DIR = REPO_ROOT / "modeloIntegrado" / "teste_500_15_365_k3"
+
+NUM_BENEFICIARIOS = 500
+NUM_MANANCIAIS = 15
+NUM_DIAS = 365
+CAPACIDADE_CAMINHAO = 13.0
+
+ARQUIVO_BENEFICIARIOS = DATA_ROOT / "Beneficiarios_RN_Ativos1.csv"
+ARQUIVO_DATAS = DATA_ROOT / "datas.csv"
+ARQUIVO_CALENDARIOS = DATA_ROOT / "CalendariosObrigatorios.csv"
+ARQUIVO_ROTAS = DATA_ROOT / "rotas"
+
+HEURISTICA_ALOCACAO = REPO_ROOT / "alocacao" / "heuristicaAlocacaoArgs.py"
+SCRIPT_MINPICOS = TEST_DIR / "minimizaPicos_500_365_p00.jl"
+SCRIPT_M2 = TEST_DIR / "m2_minpicos_500_15.jl"
+SCRIPT_MODELO_INTEGRADO = TEST_DIR / "modeloIntegrado_ws_500_15_72h.jl"
+
+
+def run_command(cmd, cwd=TEST_DIR):
+    print(f"\n>>> Executando: {' '.join(str(part) for part in cmd)}")
+    subprocess.run(cmd, cwd=str(cwd), check=True, shell=(os.name == "nt"))
+
+
+def gerar_calendario_heuristico(modo: str) -> Path:
+    beneficiarios = pd.read_csv(ARQUIVO_BENEFICIARIOS).head(NUM_BENEFICIARIOS)
+    dias_uteis = pd.read_csv(ARQUIVO_DATAS).head(NUM_DIAS)
+    calendarios = pd.read_csv(ARQUIVO_CALENDARIOS).head(NUM_DIAS)
+
+    consumo_diario = (beneficiarios["Pessoas_Atendidas"] * 0.02).round(2).values
+    capacidade_cisterna = beneficiarios["Capacidade"].values.astype(float)
+
+    autonomia = capacidade_cisterna / consumo_diario
+    quebra4 = autonomia < 5
+    quebra2 = autonomia < 3
+
+    is_dia_util = dias_uteis.iloc[:, 0].values == 1
+    carnaval_obrigatorio = calendarios["carnaval"].values == 1
+    lil_obrigatorio = calendarios["lil"].values == 1
+
+    entregas = np.zeros((NUM_BENEFICIARIOS, NUM_DIAS), dtype=int)
+    volume_atual = capacidade_cisterna.copy()
+
+    for k in range(NUM_DIAS):
+        volume_atual -= consumo_diario
+        entregas_hoje = np.zeros(NUM_BENEFICIARIOS, dtype=int)
+
+        if is_dia_util[k]:
+            cond_obrigatoria = (carnaval_obrigatorio[k] & quebra4) | (lil_obrigatorio[k] & quebra2)
+            entregas_hoje = np.where(cond_obrigatoria, 1, 0)
+
+            if modo == "full":
+                espaco_livre = capacidade_cisterna - (volume_atual + entregas_hoje * CAPACIDADE_CAMINHAO)
+                caminhoes_extras = np.maximum(0, espaco_livre // CAPACIDADE_CAMINHAO)
+                entregas_hoje += caminhoes_extras.astype(int)
+
+            nao_uteis_consecutivos = 0
+            idx_check = k + 1
+            while idx_check < NUM_DIAS and not is_dia_util[idx_check]:
+                nao_uteis_consecutivos += 1
+                idx_check += 1
+
+            consumo_ate_proximo_util = consumo_diario * (1 + nao_uteis_consecutivos)
+            volume_projetado = volume_atual + (entregas_hoje * CAPACIDADE_CAMINHAO)
+            precisa_emergencia = volume_projetado < consumo_ate_proximo_util
+            entregas_hoje = np.where(precisa_emergencia, entregas_hoje + 1, entregas_hoje)
+
+        volume_atual += entregas_hoje * CAPACIDADE_CAMINHAO
+        volume_atual = np.clip(volume_atual, 0, capacidade_cisterna)
+        entregas[:, k] = entregas_hoje
+
+    df_saida = pd.DataFrame(entregas, columns=range(1, NUM_DIAS + 1))
+    df_saida.insert(0, "Beneficiarios", beneficiarios.index + 1)
+
+    output = TEST_DIR / f"abastecimento_heu_{modo}.csv"
+    df_saida.to_csv(output, index=False)
+    print(f"Calendario heuristico salvo: {output}")
+    return output
+
+
+def rodar_heuristica_alocacao(caminho_abastecimento: Path, sufixo: str) -> None:
+    output_aloc = TEST_DIR / f"alocacao_heu_{sufixo}.csv"
+    output_custos = TEST_DIR / f"custos_heu_{sufixo}.csv"
+
+    run_command([
+        "python",
+        str(HEURISTICA_ALOCACAO),
+        str(caminho_abastecimento),
+        str(output_aloc),
+        str(output_custos),
+        str(ARQUIVO_ROTAS),
+        str(NUM_MANANCIAIS),
+    ], cwd=REPO_ROOT)
+
+
+def main():
+    TEST_DIR.mkdir(parents=True, exist_ok=True)
+
+    print("=== Etapa 1: heuristicas de calendario ===")
+    caminho_full = gerar_calendario_heuristico("full")
+    caminho_limite = gerar_calendario_heuristico("limite")
+
+    print("=== Etapa 2: heuristica de alocacao ===")
+    rodar_heuristica_alocacao(caminho_full, "full")
+    rodar_heuristica_alocacao(caminho_limite, "limite")
+
+    print("=== Etapa 3: minimizaPicos p=0.00 ===")
+    run_command(["julia", str(SCRIPT_MINPICOS)])
+
+    print("=== Etapa 4: m2 sobre o melhor calendario do minimizaPicos ===")
+    run_command(["julia", str(SCRIPT_M2)])
+
+    print("=== Etapa 5: modelo integrado com warm start e limite de 72h ===")
+    run_command(["julia", str(SCRIPT_MODELO_INTEGRADO)])
+
+    print("\nFluxo concluido.")
+
+
+if __name__ == "__main__":
+    main()
